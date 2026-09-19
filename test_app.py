bb9037deb92e1013a6b2dad74e46703c3dab6380
Ayush-Bhardwaj-cyber <ayush.stars2006@gmail.com>
2026-09-19 12:11:34 +0530

    Add API tests

---
+++ test_app.py
@@ -0,0 +1,24 @@
+from app import app
+
+def test_home():
+    client = app.test_client()
+    response = client.get("/")
+    assert response.status_code == 200
+
+def test_get_notes():
+    client = app.test_client()
+    response = client.get("/notes")
+    assert response.status_code == 200
+
+def test_add_note():
+    client = app.test_client()
+
+    response = client.post(
+        "/notes",
+        json={
+            "title": "Nmap",
+            "content": "Network scanning tool"
+        }
+    )
+
+    assert response.status_code == 201
